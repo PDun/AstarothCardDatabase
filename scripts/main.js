a1e5a6e6a1c105d5099cc692c34c4befd41b6f27
Paul Muir <pmuir@paylocity.com>
2017-09-17 18:54:00 -0400

    removing initilization of cards

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,6 @@ var app = angular.module('jsbin', []);
 
 app.controller('DemoCtrl', function ($http, $sce) {
     this.name = 'Test';
-    this.cards = [];
     
     $http.get('data/cards.json')
         .success(function(data){
