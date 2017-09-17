fd4daf8dd86d80e3816e826772082217597213fc
Paul Muir <pmuir@paylocity.com>
2017-09-17 16:13:37 -0400

    Accessing Data

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,17 @@
 var app = angular.module('jsbin', []);
 
-app.controller('DemoCtrl', function () {
+app.controller('DemoCtrl', function ($http) {
     this.name = 'Test';
+    this.cards = [];
+    $http({
+        method: 'GET',
+        url: 'https://loapk3.fingertactic.com/card.php?do=GetAllCard&phpp=FACEBOOK&phpl=EN&pvc=2.0&pvb=2016-09-13%2014%3A52%3A31&sns=KONGREGATE&origin='
+      }).then(function successCallback(response) {
+          this.cards = response.Cards
+        }, function errorCallback(response) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        });
 });
+
+
